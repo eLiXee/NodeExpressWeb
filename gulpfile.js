e1d60cbfd6b1d4afb16a7bfe018d0ff49e98369a
arronnandotech <arronbq@gmail.com>
2016-12-27 16:13:31 -0500

    Add API connection

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,24 @@ var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
-gulp.task('style', function (){
+gulp.task('style', function () {
     return gulp.src(jsFiles)
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish',{
-        verbose: true
-    }))
-    .pipe(jscs());
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
 });
 
-gulp.task('inject', function (){
+gulp.task('inject', function () {
     var wiredep = require('wiredep').stream;
     var inject = require('gulp-inject');
 
     var injectSrc = gulp.src(['./public/css/*.css',
-                              './public/js/*.js'], {read: false});
+                              './public/js/*.js'], {
+        read: false
+    });
+
     var injectOptions = {
         ignorePath: '/public'
     };
@@ -27,26 +30,28 @@ gulp.task('inject', function (){
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
-        ignorePath: '../../public',
+        ignorePath: '../../public'
     };
 
-    return gulp.src('./src/views/*.jade')
-    .pipe(wiredep(options))
-    .pipe(inject(injectSrc, injectOptions))
-    .pipe(gulp.dest('./src/views'));
+    return gulp.src('./src/views/*.ejs')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+
 });
 
-gulp.task('serve', ['style', 'inject'], function(){
+gulp.task('serve', ['style', 'inject'], function () {
     var options = {
         script: 'app.js',
         delayTime: 1,
         env: {
             'PORT': 3000
-          },
+        },
         watch: jsFiles
-      };
+    };
+
     return nodemon(options)
-    .on('restart', function(ev){
-        console.log('Restarting....');
-      });
-    });
+        .on('restart', function (ev) {
+            console.log('Restarting....');
+        });
+});
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,58 +1,21 @@
 var express = require('express');
-
 var bookRouter = express.Router();
-var sql = require('mssql');
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 
-var router = function(nav){
-var books = 
-[{
-    title: 'IT',
-    genre: 'Horror',
-    author: 'Stephen King',
-    read: false
-},
-{
-    title: 'C# IN DEPTH',
-    genre: 'Programming',
-    author: 'Manning',
-    read: false
-},
-{
-    title: 'Creating Mobile Apps with Xamarin.Forms',
-    genre: 'Programming',
-    author: 'Charles Petzold',
-    read: false
-}
-];
-bookRouter.route('/')
-.get(function (req, res){
-    var request = new sql.Request();
-    request.query('select * from books', 
-    function(err, recordset){
-        console.log(recordset);
-    })
-    res.render('booksListView', 
-        {
-        title: 'Books' ,
-        nav: nav,
-        books: books
-    });        
-    });
+var router = function (nav) {
+    var bookService = require('../services/goodreadsService')();
+var bookController = require('../controllers/bookController')(bookService,nav);
+    bookRouter.use(bookController.middleware);
+    bookRouter.route('/')
+    .get(bookController.getIndex);
+     
 
+    bookRouter.route('/:id')
+    .get(bookController.getById);
+       
 
-bookRouter.route('/:id')
-.get(function (req, res){
-    var id = req.params.id
-      res.render('bookView', 
-        {
-        title: 'Books' ,
-        nav: nav,
-         book: books[id]
-       });
-        
-});
     return bookRouter;
-}
-
+};
 module.exports = router;
